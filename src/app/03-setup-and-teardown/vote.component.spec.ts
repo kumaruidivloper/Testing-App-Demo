bfca2a64755fe0189bcf2c714b5d6acf7b601b14
kumarshan <kumaruidivloper@gmail.com>
2019-07-26 17:54:04 +0530

    upVote-downVote-test

--- src/app/03-setup-and-teardown/vote.component.spec.ts
+++ src/app/03-setup-and-teardown/vote.component.spec.ts
@@ -1,9 +1,35 @@
 import { VoteComponent } from './vote.component';
 
 describe('VoteComponent', () => {
-  it('', () => {
+  let component: VoteComponent;
+  beforeEach(() => {
+    component = new VoteComponent();
   });
 
-  it('', () => {
+  afterEach(() => {});
+  // beforeAll();
+  // afterAll();
+
+  it('should increment totalVotes when upvoted', () => {
+    // Arrange
+    // const component = new VoteComponent();
+
+    // Act
+    component.upVote();
+
+    // Assert
+    expect(component.totalVotes).toBe(1);
   });
+
+  it('should decrement totalVotes when downvotes', () => {
+    // Arrange
+    // const component = new VoteComponent();
+
+    // Act
+    component.downVote();
+
+    // Assert
+    expect(component.totalVotes).toBe(-1);
+  });
+
 });
