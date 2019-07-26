ba1a7b9cf850c8fe74ae296961594b280c16cb7f
kumarshan <kumaruidivloper@gmail.com>
2019-07-26 16:22:47 +0530

    Unit-Testing-Fundamentals

---
+++ src/app/01-fundamentals/compute.ts
@@ -0,0 +1,6 @@
+export function compute(numberr: any) {
+    if (numberr < 0) {
+      return 0;
+    }
+    return numberr + 1;
+}
---
+++ src/app/04-forms/todo-form.component.spec.ts
@@ -0,0 +1,15 @@
+import { TodoFormComponent } from './todo-form.component';
+
+describe('TodoFormComponent', () => {
+let component: TodoFormComponent;
+
+beforeEach(() => {
+  });
+
+it('', () => {
+});
+
+it('', () => {
+});
+
+});
